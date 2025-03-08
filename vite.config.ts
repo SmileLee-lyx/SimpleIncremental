10acf998458871a5a1f80339d3ebf4cbec21232d
Smile <smileleelyx@qq.com>
2025-03-09 03:05:46 +0800

    add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfill'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    nodePolyfills({
+      // 根据需要配置 polyfill
+      globals: {
+        Buffer: true, // 启用 Buffer polyfill
+        global: true, // 启用 global polyfill
+        process: true, // 启用 process polyfill
+      },
+    }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
